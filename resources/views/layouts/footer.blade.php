0610cdb82700f4df6c8fb8c6249b77e693199cf9
OWKASIONG <owkasiong1217@e.newera.edu.my>
2023-01-14 12:14:04 +0800

    change

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+
+footer {
+  font-weight: bold;
+  text-align: center;
+  padding: 15px;
+  background-color: #016fff;
+  color: white;
+}
+
+.fa {
+  font-size: 25px;
+  width: 50px;
+  text-align: center;
+  text-decoration: none;
+  margin: 8px 2px;
+  border-radius: 50%;
+}
+
+.fa:hover {
+    opacity: 0.7;
+}
+
+.fa-facebook {
+  background: #3B5998;
+  color: white;
+}
+
+.fa-linkedin {
+  background: #007bb5;
+  color: white;
+}
+
+.fa-instagram {
+  background: #125688;
+  color: white;
+}
+
+</style>
+</head>
+<body>
+
+<footer>
+<a href="https://www.facebook.com/ow.kasiong/" class="fa fa-facebook"></a>
+<a href="https://www.linkedin.com/in/ow-ka-siong-b24871226/" class="fa fa-linkedin"></a>
+<a href="https://www.instagram.com/ka_siongow/" class="fa fa-instagram"></a> 
+<p>OW KA SIONG 2250169-BSE</p>
+</footer>
+
+</body>
+</html>
