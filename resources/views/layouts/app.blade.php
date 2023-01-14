f5bc6d52c9ea61fd42c9c02747464f0a7b15a738
OWKASIONG <owkasiong1217@e.newera.edu.my>
2023-01-14 10:47:37 +0800

    change

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,13 +33,13 @@
     @if (Auth::check())
         <div class="navbar navbar-expand-md navbar-light bg-primary shadow-sm">
             <div class="container d-flex justify-content-between">
-                <span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; </span>
+                <span style="font-size:30px;cursor:pointer; color: white" onclick="openNav()">&#9776; </span>
                 <div>
                     <div class="d-flex justify-content-between">
                         <img src="{{ asset(Auth::user()->GetAvatar()) }}" alt="" width="40px" height="40px" class="rounded-circle">
-                        &nbsp;&nbsp;
-                        <h4 class="m-auto">{{ Auth::user()->first_name.' '.Auth::user()->last_name }}</h4>
-                        &nbsp;&nbsp;
+                        &nbsp;&nbsp;&nbsp;
+                        <h4 class="m-auto text-white">{{ Auth::user()->first_name.' '.Auth::user()->last_name }}</h4>
+                        &nbsp;&nbsp;&nbsp;
                         <form action="{{ route('logout') }}" method="POST" class="">
                             @csrf
                             <button type="submit" width="45px" height="45px" class="btn btn-danger text-white">Logout</button>
@@ -51,7 +51,7 @@
         <div id="mySidenav" class="sidenav">
             <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
 
-            <a href="{{ route('home') }}" class="text-decoration-none px-2"><h2 class="text-black text-center">NEUC_Complaint</h2></a>
+            <a href="{{ route('home') }}" class="text-decoration-none px-2"><h2 class="text-white text-center">NEUC Complaint System</h2></a>
 
             <div class="d-flex justify-content-center my-3">
                 <div style="width:200px;
@@ -64,12 +64,12 @@
             </div>
 
             <div class="text-center">
-                <h4>Name: {{ Auth::user()->first_name.' '.Auth::user()->last_name }}</h4>
+                <h4 class="text-white">Name: {{ Auth::user()->first_name.' '.Auth::user()->last_name }}</h4>
 
                 @if (Auth::user()->HasRole())
                     <div class="list-group">
                         @if (Auth::user()->IsHelpDesk())
-                            <a data-bs-toggle="collapse" href="#collapseHelpdesk" role="button" aria-expanded="false" aria-controls="collapseHelpdesk" class="list-group-item list-group-item-action h5 @if(Request::is('help-desk/*')) active @endif">Help Desk</a>
+                            <a data-bs-toggle="collapse" href="#collapseHelpdesk" role="button" aria-expanded="false" aria-controls="collapseHelpdesk" class="list-group-item list-group-item-action h5 @if(Request::is('help-desk/*')) active @endif;">Help Desk</a>
                             <div id="collapseHelpdesk" class="collapse">
                                 <a href="{{ route('helpdesk.dashboard') }}">Dashboard</a>
                                 <a href="{{ route('helpdesk.complaints.index') }}">All Complaints</a>
